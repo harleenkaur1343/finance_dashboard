91a4d3ddf50c24a8b17ab60bbc8f66e7a65a654f
Harleen Kaur <harleenkaur1343@gmail.com>
2026-04-04 02:22:28 +0530

    Add min width to prevent shrinking below size

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout() {
     <div className="flex min-h-screen">
       <Sidebar />
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header role={role} setRole={setRole} />
 
         <main className="p-6">
